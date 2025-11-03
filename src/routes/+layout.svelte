9ce94c2414fe05a3fe425d1aae46a37b6a3338db
Zachary Upshaw <zlupshaw@gmail.com>
2025-11-03 11:57:34 -0800

    Change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '../../static/icons/acorn.svg';
 	import Desktop from '$lib/components/Desktop.svelte';
 	import Bootscreen from '$lib/components/Bootscreen.svelte';
 
